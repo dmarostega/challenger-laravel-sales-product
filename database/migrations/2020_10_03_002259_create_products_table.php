452c21a99159175bf506a1cf2838721a265bab9e
Diogo <dmarostega@gmail.com>
2020-10-02 22:31:50 -0300

    DB Connection and Cretead Migrations

---
+++ database/migrations/2020_10_03_002259_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->double('price');
+            $table->integer('quantity');
+            $table->unsignedInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(('category_id'));
+        });
+        Schema::dropIfExists('products');
+    }
+}
